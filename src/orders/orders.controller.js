6c0bbb03486f88486f2a0b3cff47a9575a6b05f0
Ashley Nguyen <ashleynguyen37@gmail.com>
2023-05-21 18:59:03 -0500

    POST /orders route created

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,41 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+function list(req, res) {
+    res.json({ data: orders })
+}
+
+
+
+function orderExists(req, res, next) {
+    const { orderId } = req.params;
+    const foundOrder = orders.find((order) => order.id === orderId);
+
+    if(foundOrder) {
+        res.locals.order = foundOrder;
+        next();
+    } else {
+        next({
+            status: 404,
+            message: `Order id ${orderId} not found.`,
+        })
+    }
+}
+
+function create(req, res) {
+    const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+    const newOrder = {
+        id: nextId(),
+        deliverTo,
+        mobileNumber,
+        status,
+        dishes,
+    }
+    orders.push(newOrder);
+    res.status(201).json({ data: newOrder })
+}
+
+module.exports = {
+    list,
+}
